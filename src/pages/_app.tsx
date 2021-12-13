538388f8f0f019a0bf02d5ee380a80e625cd0639
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-13 17:29:25 -0300

    :wrench: feat: change head for application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,21 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>Boilerplate Nextjs</title>
+                <title>NikeShoes</title>
                 <link
                     rel="shortcut icon"
-                    href="favicon.ico"
+                    href="/assets/icons/Logo.svg"
                     type="image/x-icon"
                 />
-                <link rel="manifest" href="manifest.json" />
-                <meta name="description" content="Boilerplate Nextjs" />
-                <meta name="theme-color" content="#000" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Bellefair&family=DM+Sans:wght@400;500;700&family=Raleway:wght@400;500;600;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+                <meta
+                    name="description"
+                    content="NikeShoes is a fictional e-commerce focused on selling nike shoes. "
+                />
+                <meta name="theme-color" content="#070707" />
             </Head>
             <GlobalStyles />
             <Component {...pageProps} />
