3bcff00f5c557e9537249b608ff4e902e6af5568
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-14 00:51:15 -0300

    :art: style: add variables and new font

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,6 +2,16 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
+:root {
+    --black-transparent:  #000000bf;
+
+    --x-small: 1.3rem;
+    --small: 2.0rem;
+    --medium: 4.8rem;
+    --large: 6rem;
+}
+
+
 * {
     margin: 0;
     padding: 0;
@@ -14,7 +24,11 @@ html {
 }
 
 body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Raleway,--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+a {
+    color:#fff;
 }
 
 `;
