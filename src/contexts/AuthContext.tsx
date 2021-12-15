6b52f603637dd4dac9f5e948a976e95781972263
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-14 23:16:25 -0300

    :white_check_mark: feat: create context for authentication

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,53 @@
+import Router from "next/router";
+import { setCookie, parseCookies } from "nookies";
+import { createContext, useEffect } from "react";
+
+import { errorToast } from "../helpers/toast";
+import { api } from "../services/api";
+
+interface ISignInData {
+    email: string;
+    password: string;
+}
+
+interface IAuthContext {
+    signIn: (data: ISignInData) => Promise<void>;
+}
+
+interface IChildren {
+    children: JSX.Element;
+}
+
+export const AuthContext = createContext({} as IAuthContext);
+
+export function AuthProvider({ children }: IChildren) {
+    useEffect(() => {
+        const { "nikeshoes.token": token } = parseCookies();
+
+        if (token) {
+            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+        }
+        if (!token) Router.push("/");
+    }, []);
+
+    async function signIn({ email, password }: ISignInData) {
+        await api
+            .post("users/login", { email, password })
+            .then(response => {
+                setCookie(undefined, "nikeshoes-token", response.data, {
+                    maxAge: 60 * 60 * 1, // 1 hour
+                });
+                Router.push("/store");
+            })
+            .catch(error => {
+                console.log(error);
+                errorToast(error.response.data.error);
+            });
+    }
+
+    return (
+        <AuthContext.Provider value={{ signIn }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
