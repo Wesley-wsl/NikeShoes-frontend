2b443460edf14a00bc84c7f9aabb559bb51d1f9b
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-15 00:27:41 -0300

    :wrench: feat: ensure that token is valid before redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-import SignIn from "../templates/SignIn";
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
+
+import { getApiClient } from "../services/apiSSR";
+import Account from "../templates/Account";
 
 export default function Home() {
-    return <SignIn />;
+    return <Account />;
 }
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const { "nikeshoes-token": token } = parseCookies(ctx);
+    const validJwt = await getApiClient(ctx)
+        .get(`users/validjwt/${token}`)
+        .then(() => true)
+        .catch(() => false);
+
+    if (token && validJwt) {
+        return {
+            redirect: {
+                destination: "/store",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    };
+};
