93de0c7c05f3fc924e755e9cd309d5021f9dd784
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-15 17:06:37 -0300

    :art: style: Login and Register

---
+++ src/styles/sharedStyle.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const Account = styled.section`
+    width: 25rem;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: 5rem auto;
+
+    > div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 2.5rem;
+    }
+
+    h1 {
+        font-size: var(--medium);
+    }
+
+    form {
+        div {
+            position: relative;
+            input {
+                background-color: transparent;
+                border-bottom: 1px solid #fff;
+                padding: 1rem;
+                color: #fff;
+                width: 100%;
+                padding-right: 3rem;
+                margin-bottom: 2.5rem;
+                outline: none;
+            }
+
+            input::placeholder {
+                color: #fff9;
+                font-size: 1.7rem;
+            }
+
+            .icon {
+                position: absolute;
+                top: 10%;
+                right: 0;
+            }
+
+            .password {
+                cursor: pointer;
+            }
+        }
+
+        button {
+            padding: 0.9rem;
+            width: 100%;
+            border-radius: 0.6rem;
+            margin-bottom: 2rem;
+            cursor: pointer;
+            font-size: 1.8rem;
+            font-weight: 600;
+            transition: 0.2s ease-in;
+
+            &:hover {
+                background-color: #000;
+                color: #fff;
+            }
+        }
+    }
+
+    > p {
+        font-size: var(--x-small);
+    }
+`;
