ffd0ea4d684084075e56b9336c0e66b2d011962f
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-19 16:15:22 -0300

    :wrench: feat: create page store
    This Page is the store where you can see all products filtered by category for the user to add to the cart and make the purchase.

---
+++ src/pages/store/index.tsx
@@ -0,0 +1,166 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import { parseCookies } from "nookies";
+import { useState, useEffect } from "react";
+
+import ArrowLeft from "../../../public/assets/icons/ArrowLeft.svg";
+import ArrowRight from "../../../public/assets/icons/ArrowRight.svg";
+import { IProductsArray, IProductsSeparated } from "../../@types";
+import Header from "../../components/Header";
+import SizeSelector from "../../components/SizeSelector";
+import { errorToast, successToast } from "../../helpers/toast";
+import { api } from "../../services/api";
+import { getApiClient } from "../../services/apiSSR";
+import * as S from "../../styles/pages/Store";
+
+const Store: React.FC<IProductsSeparated> = ({
+    man,
+    woman,
+    isAdmin,
+    countCart,
+}) => {
+    const [isMan, setIsMan] = useState(true);
+    const [products, setProducts] = useState<any>();
+    const [indexProducts, setIndexProducts] = useState(0);
+    const [cartQuantity, setCartQuantiity] = useState(countCart);
+
+    function nextProduct() {
+        if (indexProducts + 1 > products.length - 1) return;
+        setIndexProducts(indexProducts + 1);
+    }
+
+    function previousProduct() {
+        if (indexProducts - 1 < 0) return;
+        setIndexProducts(indexProducts - 1);
+    }
+
+    async function handleAddProductInCart() {
+        await api
+            .post(`cart/${products[indexProducts]._id}`)
+            .then(() => {
+                successToast("Product added in your cart");
+                setCartQuantiity(cartQuantity + 1);
+            })
+            .catch((error: { response: { data: { error: string } } }) =>
+                errorToast(error.response.data.error),
+            );
+    }
+
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+        setIndexProducts(0);
+        if (isMan === true) setProducts(man);
+        if (isMan === false) setProducts(woman);
+    }, [isMan, man, woman]);
+
+    return (
+        <>
+            <Header
+                setIsMan={setIsMan}
+                isAdmin={isAdmin}
+                cartQuantity={cartQuantity}
+            />
+            <S.Container>
+                {products ? (
+                    <>
+                        <S.ProductInformation data-aos="flip-right">
+                            <div>
+                                <p>{isMan ? "Men's Shoe" : "Women's Shoe"}</p>
+                                <h1>{products[indexProducts].name}</h1>
+
+                                <p>{products[indexProducts].description}</p>
+                                {products[indexProducts].video_url && (
+                                    <>
+                                        <S.Button>
+                                            <a
+                                                href={
+                                                    products[indexProducts]
+                                                        .video_url
+                                                }
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                PLAY VIDEO
+                                            </a>
+                                        </S.Button>
+                                    </>
+                                )}
+                            </div>
+                            <SizeSelector />
+                        </S.ProductInformation>
+
+                        <S.Product className="product" data-aos="flip-right">
+                            <span className="img">
+                                <Image
+                                    src={products[indexProducts].product_image}
+                                    width="600"
+                                    height="600"
+                                />
+                            </span>
+                        </S.Product>
+
+                        <S.Manager>
+                            <div>
+                                <div onClick={previousProduct}>
+                                    <p>Prev</p>
+                                    <ArrowLeft />
+                                </div>
+                                <div onClick={nextProduct}>
+                                    <p>Next</p>
+                                    <ArrowRight />
+                                </div>
+                            </div>
+
+                            <S.Button onClick={handleAddProductInCart}>
+                                Add to cart - ${products[indexProducts].price}
+                            </S.Button>
+                        </S.Manager>
+                    </>
+                ) : (
+                    "Loading..."
+                )}
+            </S.Container>
+        </>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const { "nikeshoes-token": token } = parseCookies(ctx);
+    let userID;
+
+    const validJwt = await getApiClient(ctx)
+        .get(`users/validjwt/${token}`)
+        .then((response: { data: { id: string } }) => {
+            userID = response.data.id;
+            return true;
+        })
+        .catch(() => false);
+
+    if (!token || !validJwt) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    const woman = await getApiClient(ctx).get("products?category=Woman");
+    const man = await getApiClient(ctx).get("products?category=Man");
+    const user = await getApiClient(ctx).get(`users/${userID}`);
+    const isAdmin = user.data.user[0]["admin"];
+    const countCart = user.data.user[0]["cart"].length;
+
+    return {
+        props: {
+            woman: woman.data.listProducts,
+            man: man.data.listProducts,
+            isAdmin,
+            countCart,
+        },
+    };
+};
+
+export default Store;
