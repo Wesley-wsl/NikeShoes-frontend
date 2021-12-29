be23a760e7c6b55019fe20836a19a13d0cb02493
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-29 16:00:16 -0300

    :white_check_mark: feat: add baseURL for api in deploy

--- src/services/apiSSR.ts
+++ src/services/apiSSR.ts
@@ -6,7 +6,11 @@ export function getApiClient(ctx?: GetServerSidePropsContext) {
     const { "nikeshoes-token": token } = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: "http://localhost:3333/",
+        baseURL: `${
+            process.env.NEXT_PUBLIC_API_URL
+                ? process.env.NEXT_PUBLIC_API_URL
+                : "http://localhost:3333/"
+        }`,
     });
 
     if (token) {
